9bff89a41f93b9e0ad70deea238d88f8a13bd03c
vbatti4636 <valeriebattista@verizon.net>
2022-03-18 12:55:58 -0400

    Created dropdown menu for tags when adding song

--- urmusic-1.0-beta/scripts/add.js
+++ urmusic-1.0-beta/scripts/add.js
@@ -1,8 +1,17 @@
 // TODO: When a song is added here, it will also be added on our server
 
+var tagsToAdd = [];
+
 //Function to display the add song popup
 function openAddPopup() {
     document.getElementById("add-popup").style.display = "inline-block";
+}
+
+//Function to clear tags information
+function clearTagInput() {
+    document.getElementById("tag-dropdown-input").value = "";
+    document.getElementById("tags-to-add").textContent = "(no tags have been added)";
+    this.tagsToAdd = [];
 }
 
 //Function to clear input fields
@@ -11,7 +20,7 @@ function clearInputFields() {
     document.getElementById("artist-input").value = "";
     document.getElementById("album-input").value = "";
     document.getElementById("link-input").value = "";
-    document.getElementById("tag-input").value = "";
+    clearTagInput();
 }
 
 //Function to cancel adding a song
@@ -33,8 +42,7 @@ function saveAdd() {
     var artistInput = document.getElementById("artist-input").value;
     var albumInput = document.getElementById("album-input").value;
     var linkInput = document.getElementById("link-input").value;
-    var tagsInput = document.getElementById("tag-input").value;
-    
+
     //Ensure required fields are filled out
     
     //TODO: Display error message to the user
@@ -52,8 +60,10 @@ function saveAdd() {
         console.log("Saving song: " + songTitleInput);
         console.log("Artist: " + artistInput);
         console.log("Album: " + albumInput);
+
+        //TODO: confirm that link is valid?
         console.log("Link: " + linkInput);
-        console.log("Tags: " + tagsInput);
+        console.log("Tags: " + tagsToAdd);
 
         clearInputFields();
 
@@ -102,4 +112,63 @@ function tagsToggle() {
     viewMoreText = "+ View tags";
     dismissText = "- Dismiss tags";
     toggleView(buttonId, divId, viewMoreText, dismissText);
+}
+
+//Function to show dropdown menu options when adding tags
+function showDropdownOptions() {
+    var dropdownOptions = document.getElementById("tag-dropdown-options");
+    dropdownOptions.style.display = "block";
+    dropdownOptions.addEventListener("mousedown",
+    function(event) {
+        event.preventDefault();
+    });
+
+}
+//Function to hide dropdown menu options when adding tags
+function hideDropdownOptions() {
+    document.getElementById("tag-dropdown-options").style.display = "none";
+}
+
+//Function to search for tags in the dropdown menu
+//Referenced https://www.w3schools.com/howto/howto_js_filter_dropdown.asp
+function filterTagSearch() {
+    var input = document.getElementById("tag-dropdown-input");
+    var uppercaseInput = input.value.toUpperCase();
+    var div = document.getElementById("tag-dropdown");
+    p = div.getElementsByTagName("p");
+
+    for (i = 0; i < p.length; i++) {
+        textValue = p[i].textContent || p[i].innerText;
+        if (textValue.toUpperCase().indexOf(uppercaseInput) > -1) {
+            p[i].style.display = "";
+        } else {
+            p[i].style.display = "none"
+        }
+    }
+}
+
+//Function to add a tag from the dropdown menu to the list
+function addExistingTag(tagText) {
+
+    //Check if tag has already been added
+    if (tagsToAdd.includes(tagText)) {
+
+        //TODO: Only display tags that haven't already been added
+        //Also should prevent the user from typing in a tag they have already added
+        console.log("Error: this tag has already been added");
+    } else {
+
+        //Add tag to list
+        tagsToAdd.push(tagText);
+
+        //Display updated list of tags
+        var tagList = tagsToAdd.toString();
+        tagList = tagList.replaceAll(",", ", ");
+        document.getElementById("tags-to-add").textContent = tagList;
+
+        //Hide dropdown options and clear input field
+        document.getElementById("tag-dropdown-options").value = "";
+        document.getElementById("tag-dropdown-input").blur();
+        hideDropdownOptions();
+    }
 }
